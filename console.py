863a276cdd28c82c0916c95bfa117112a4c53bcc
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-27 21:41:21 +0000

    9 and 10

--- console.py
+++ console.py
@@ -4,13 +4,18 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 def _split(line):
     return line.split()
 
 
-classes = ['BaseModel', 'User']
+classes = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
 
 class HBNBCommand(cmd.Cmd):
