a5b6f26aa13988fa3d7919f94c76301ac0adab34
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-27 21:19:36 +0000

    check some

--- models/user.py
+++ models/user.py
@@ -4,8 +4,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """the class User that inherits from BaseModel"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """the class User that inherits from BaseModel
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
