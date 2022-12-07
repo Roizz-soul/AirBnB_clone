3e8c7994a248ec920975ce898b11ef876e91c55b
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-12-07 11:54:37 +0000

    some stuffs

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,11 +7,6 @@ import unittest
 
 class TestBaseModel(unittest.TestCase):
     """Tests the basemodel attributes and methods"""
-    def SetUp(self):
-        """A setup method that creates instance objects"""
-        d1 = BaseModel()
-        sleep(0.05)
-        d2 = BaseModel()
     
     """
     def tearDown(self):
@@ -21,20 +16,32 @@ class TestBaseModel(unittest.TestCase):
 
     def test_id(self):
         """Tests for the value of id"""
+        d1 = BaseModel()
+        sleep(0.05)
+        d2 = BaseModel()
         self.assertNotEqual(d1.id, d2.id)
 
     def test_created_at(self):
         """Tests for the value of created_at"""
+        d1 = BaseModel()
+        sleep(0.05)
+        d2 = BaseModel()
         self.assertNotEqual(d1.created_at, d2.created_at)
 
     def test_save(self):
         """A test to check if the program saves"""
+        d1 = BaseModel()
+        sleep(0.05)
+        d2 = BaseModel()
         d1.save()
-        with open("file.JSON", "r") as f:
+        with open("file.json", "r") as f:
             self.assertIn(d1.id, f.read())
 
     def test_to_dict(self):
         """A test to check if the to_dict functions"""
+        d1 = BaseModel()
+        sleep(0.05)
+        d2 = BaseModel()
         our_Dict = d1.to_dict()
         self.assertIn('__class__', our_Dict.keys())
 
@@ -42,6 +49,9 @@ class TestBaseModel(unittest.TestCase):
         """A test to checks for the string representation
             of an instance
         """
+        d1 = BaseModel()
+        sleep(0.05)
+        d2 = BaseModel()
         our_Str = d1.__str__()
         self.assertIn("[BaseModel]", our_Str)
 
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,12 +1,29 @@
-#!/usr/bin/python3
-"""Module to test the FileStorage class"""
+#!/usr/bin/oython3
+"""Test module for FileStorage class"""
+import json
+import unittest
+import models
+from time import sleep
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from time import sleep
-import unittest
 
 
-
-class TestBaseModel(unittest.TestCase):
-    """Tests the basemodel attributes and methods"""
+class TestFileStorage(unittest.TestCase):
+    """Tests for fileStorage attributes and methods"""
     def SetUp(self):
+        """Creates object instances"""
+        d1 = BaseModel()
+        sleep(0.05)
+        d2 = BaseModel()
+
+    def test_object(self):
+        with self.assertRaises(AttributeError):
+            FileStorage.__objects
+
+    def test_file_path(self):
+        with self.assertRaises(AttributeError):
+            FileStorage.__file_path
+
+
+if __name__ == '__main__':
+    unittest.main()
